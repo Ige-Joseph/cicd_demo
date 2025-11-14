d0f5a347b717776fe509d130f6cbd097cb367afc
Joseph <Joseph>
2025-11-14 11:47:03 +0100

    initial ci/cd setup

---
+++ tasks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import (
+    TaskListCreateView,
+    TaskRetrieveUpdateDeleteView
+)
+
+urlpatterns = [
+    
+    path('', TaskListCreateView.as_view(), name='task-list'),
+    path('<int:pk>/', TaskRetrieveUpdateDeleteView.as_view(), name='task-detail'),
+]
